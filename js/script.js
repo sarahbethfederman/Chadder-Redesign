42f51798975f251aa7426864bcce5b6372ce0be8
Sarah Federman <sarahbethfederman@gmail.com>
2015-01-04 02:17:10 -0500

    fixed typos, made a contact form

--- js/script.js
+++ js/script.js
@@ -28,7 +28,7 @@
   var Modal = function($trigger, triggerEvent) {
     var Modal = function() {
       this.trigger = $trigger;                                            // element that triggers modal
-      this.contentRef = $trigger.data('content');                         // name of content key in JSON
+      this.contentRef = $trigger.data('content');                         // data-content is name of content key in JSON
       this.isActive = false;                                              // is the modal currently active
 
       if (triggerEvent) {                                                 // event that triggers modal
@@ -71,26 +71,51 @@
     });
   };
 
-  var contactForm = function($formEl) {
-    function validate() {
+  var contactForm = function($formEl, $formMessages) {
+    var $form = $formEl;
+    var $messages = $formMessages;
 
-    };
+    $form.submit(function(event) {
+      event.preventDefault();                  // Stop the browser from submitting
 
-    $formEl.on("submit", function() {                             // attach client side validation
-        if (validate()) {                                         // if it validates, submit it (via AJAX)
+      var formData = $form.serialize();
 
+      $.ajax({                                  // Submit the form using AJAX.
+        type: 'POST',
+        url: $form.attr('action'),
+        data: formData
+      }).done(function(response) {              // on success handler
+        $messages.removeClass('error');         // Make sure that the formMessages div has the 'success' class.
+        $messages.addClass('success');
+
+        $messages.text(response);               // Set the message text
+
+        $('[name="name"]').val('');             // Clear the form
+        $('[name="email"]').val('');
+        $('[name="message"]').val('');
+      }).fail(function(data) {                  // on failure handler
+        $messages.removeClass('success');       // Make sure that the formMessages div has the 'error' class
+        $messages.addClass('error');
+
+        if (data.responseText !== '') {        // Set the message text
+          $$messages.text(data.responseText);
         } else {
-          return false;                                           // don't submit if it doesn't validate
+          $$messages.text('Oops! An error occured and your message could not be sent.');
         }
-    });
-  };
+      }); // end fail
+
+    }); // end submit
+  }; // end contactForm
+
 
 
   $(document).ready(function() {
     // hook up DOM elements
     var $navBar = $('.nav-bar'),
         $form = $('.contact-form'),
+        $formMessages = $('#form-messages'),
         $modalTriggers = $('.js-modal');
+      //  console.log($modalTriggers);
 
     // init fixed nav effect
     navEffect($navBar);
@@ -99,12 +124,15 @@
     smoothScroll($navBar);
 
     // init modals
-    $modalTriggers.each(function($el) {
-      new Modal($el);
-    });
+    // $modalTriggers.each(function(index) {
+    //
+    //   console.log($(this));
+    //   new Modal($(this));
+    // });
+
 
     // init contactForm
-    contactForm($form);
+    contactForm($form, $formMessages);
   });
 
 })();
